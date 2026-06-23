b5b679959b7560124547d89c461cc28638c2dd32
BlackBird_6 <blackbird6366@gmail.com>
2026-06-23 10:53:41 -0400

    day 1

---
+++ 2025/inputs/2025_Day01-1.py
@@ -0,0 +1,28 @@
+import random
+import math
+from itertools import *
+from collections import *
+
+import time
+start_time = time.time()
+
+input = open("inputs/2025_01.txt", "r+").read().splitlines()
+
+current = 50
+total = 0
+
+for l in input:
+    mult = 1 if l[0] == "R" else -1
+    current += mult * int(l[1:])
+    current = current % 100
+
+    print(l, current)
+    if current == 0:
+        total += 1
+
+print(total)
+
+# 833
+
+
+print(f"Time elapsed: {time.time() - start_time}s")
---
+++ 2025/inputs/2025_Day01-2.py
@@ -0,0 +1,30 @@
+import random
+import math
+from itertools import *
+from collections import *
+
+import time
+start_time = time.time()
+
+input = open("inputs/2025_01.txt", "r+").read().splitlines()
+
+current = 50
+total = 0
+
+for l in input:
+    mult = 1 if l[0] == "R" else -1
+    
+    for i in range(int(l[1:])): # brute force solution (sum of terms is 597303)
+        current += mult
+        current = current % 100
+
+        if current == 0:
+            total += 1
+
+print(total)
+
+# 1200
+# 833
+# 640
+
+print(f"Time elapsed: {time.time() - start_time}s")
